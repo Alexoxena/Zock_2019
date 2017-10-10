1a9ab727eca17c73f4709f7547ec34163c041524
Alexandra.von.Trotha <Alexandra.von.Trotha@SK-448A5BCB8A90.2631.skool.net>
2017-10-10 15:36:29 +0200

    Programm

---
+++ JavaWillNurSpielen/src/zock_2019/TestVertex.java
@@ -0,0 +1,20 @@
+package zock_2019;
+
+public class TestVertex {
+	
+	public static void main(String[]args){
+
+		Vertex v1 = new Vertex (2,2);
+		System.out.println(v1);
+		Vertex v2 = v1.skalarMult(3);
+
+			
+		System.out.println(v2);
+		
+		
+		System.out.println(v1.length());
+		
+
+	}
+
+	}
--- JavaWillNurSpielen/src/zock_2019/Vertex.java
+++ JavaWillNurSpielen/src/zock_2019/Vertex.java
@@ -1,6 +1,48 @@
+package zock_2019;
 
 public class Vertex {
-public static void main(String[]args){
-	System.out.println("Hello World");
+	public double x;
+	public double y;
+
+	public Vertex (double x, double y){
+	this.x = x; //this = eigene Variable, ohne this= fremde
+	this.y = y;
+		
+	}
+	public String toString(){
+		
+		return "("+x+","+y+")";
+
+	}
+
+	public double length(){
+
+		return Math.sqrt(x*x + y * y );
+	}
+
+	public Vertex skalarMult(double s){
+
+		return new Vertex (x*s, y*s);
+
+	}
+	public void skalarMultMod(double s){
+ 
+		x=x*s;
+		y=y*s;
+	
+	}
+public Vertex add(Vertex v){
+	return new Vertex (v.x+x, v.y+y);
+	}
+
+public void addMod(Vertex v){
+	x=x+v.x;
+	y=y+v.y;
+}
+public void setzeX (double x){
+	this.x=x;
+}
+public void setzeY(double y){
+this.y=y;
 }
 }
