56fa40c4ed0fe5d67a11d0cd7dd4d5e668bdaadf
Alexandra.von.Trotha <Alexandra.von.Trotha@SK-448A5BCB8A89.2631.skool.net>
2017-10-13 08:45:05 +0200

    Erweiterung, noch Kommentare hinzufügen

--- JavaWillNurSpielen/src/zock_2019/TestVertex.java
+++ JavaWillNurSpielen/src/zock_2019/TestVertex.java
@@ -4,14 +4,20 @@ public class TestVertex {
 	
 	public static void main(String[]args){
 
-		Vertex v1 = new Vertex (2,2);
+		Vertex v1 = new Vertex (7,3);
 		System.out.println(v1);
-		Vertex v2 = v1.skalarMult(3);
-
-			
+		System.out.println(v1.length());
+		
+		Vertex v2 = v1.skalarMult(5);			
 		System.out.println(v2);
 		
+		v1.addMod(v2);
+		System.out.println(v1);
 		
+		Vertex v3 = v1.add(v2);
+			
+		System.out.println(v3);
+
 		System.out.println(v1.length());
 		
 
--- JavaWillNurSpielen/src/zock_2019/Vertex.java
+++ JavaWillNurSpielen/src/zock_2019/Vertex.java
@@ -6,43 +6,86 @@ public class Vertex {
 
 	public Vertex (double x, double y){
 	this.x = x; //this = eigene Variable, ohne this= fremde
-	this.y = y;
-		
+	this.y = y;		
 	}
-	public String toString(){
-		
+	
+	public String toString(){		
 		return "("+x+","+y+")";
-
 	}
 
 	public double length(){
-
 		return Math.sqrt(x*x + y * y );
 	}
 
 	public Vertex skalarMult(double s){
-
 		return new Vertex (x*s, y*s);
-
 	}
+	
 	public void skalarMultMod(double s){
- 
 		x=x*s;
-		y=y*s;
+		y=y*s;	
+	}
+
+	public Vertex add(Vertex v){	
+		return new Vertex (v.x+x, v.y+y);
+	}
+
+
+	public void addMod(Vertex v){	
+		x=x+v.x;	
+		y=y+v.y;
+	}
+
+	public Vertex sub(Vertex v){		
+		return new Vertex (v.x-x, v.y-y);
+	}
+
+
+	public void subMod(Vertex v){	
+		x=x-v.x;
+		y=y-v.y;
+	}
+	
+	public void setzeX (double x){	
+		this.x=x;
+	}
+
+	public void setzeY(double y){
+		this.y=y;
+	}
+
+
+	public double getX(){	
+		return x;
+	}
+	
+
+	public double getY(){	
+		return y;
+	}
+
+	public boolean equals (Object thatObject){
+	
+		if (thatObject instanceof Vertex){
+			Vertex that = (Vertex) thatObject; //Klammern = Cast , macht für Compiler klar, das thatObject Vertex ist
+			return this.x == that.x && this.y == that.y;
+		}
+		
+		return false;//nach return wird abgebrochen, d.h wenn true Methode zuende
 	
 	}
-public Vertex add(Vertex v){
-	return new Vertex (v.x+x, v.y+y);
-	}
-
-public void addMod(Vertex v){
-	x=x+v.x;
-	y=y+v.y;
-}
-public void setzeX (double x){
-	this.x=x;
-}
-public void setzeY(double y){
-this.y=y;
-}
-}
+		
+	public double distance (Vertex v){
+			double xx = this.x-v.x;
+			double yy = this.y-v.y;
+			
+		return Math.sqrt(xx*xx+yy*yy);
+		}
+		
+		public void normalize(){
+			double factor =length();
+			x=x/factor;
+			y=y/factor;			
+		}
+	
+}	
