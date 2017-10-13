d2e95d4513b959d932e28a094b812e09cc0db01f
Alexandra.von.Trotha <Alexandra.von.Trotha@SK-448A5BCB8A89.2631.skool.net>
2017-10-13 08:49:24 +0200

    TestVertex verändert

--- JavaWillNurSpielen/src/zock_2019/TestVertex.java
+++ JavaWillNurSpielen/src/zock_2019/TestVertex.java
@@ -4,21 +4,12 @@ public class TestVertex {
 	
 	public static void main(String[]args){
 
-		Vertex v1 = new Vertex (7,3);
+		Vertex v1 = new Vertex (4,76);
 		System.out.println(v1);
-		System.out.println(v1.length());
-		
-		Vertex v2 = v1.skalarMult(5);			
-		System.out.println(v2);
-		
-		v1.addMod(v2);
-		System.out.println(v1);
-		
-		Vertex v3 = v1.add(v2);
-			
-		System.out.println(v3);
 
-		System.out.println(v1.length());
+		v1.normalize();
+		System.out.println(v1);
+		System.out.println(v1.getX()+v1.getY());
 		
 
 	}
